fee36c7ed912f8fb06bf53c86de668ca5ec0f97b
Marlondot <80053997+Marlondot@users.noreply.github.com>
2021-10-15 12:31:59 -0500

    Solución 1

---
+++ src/main/java/Asiento.java
@@ -0,0 +1,12 @@
+
+public class Asiento {
+	String color;
+	int precio;
+	int registro;
+	
+	public void cambiarColor(String Color){
+		this.color=Color;
+		
+	}
+	
+}
---
+++ src/main/java/Auto.java
@@ -0,0 +1,24 @@
+
+public class Auto {
+	String modelo;
+	int precio;
+	Asiento asientos;
+	String marca;
+	Motor motor;
+	int registro;
+	static int cantidadCreados;
+	
+	int cantidadAsientos() {
+		return this.asientos.registro;
+	}
+	
+	void verificarIntegridad() {
+		if (this.asientos.registro==this.registro && this.motor.registro==this.registro) {
+			System.out.println("Las piezas no son originales");
+		}
+		else {
+			System.out.println("Auto original");
+		}
+	}
+
+}
